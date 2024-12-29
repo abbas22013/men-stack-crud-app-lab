6f108a44eb4b8b6f6faaff97acca31f1bc8de820
abbas <abbas22013@gmail.com>
2024-12-29 23:33:38 +0300

    Finish

--- server.js
+++ server.js
@@ -134,7 +134,12 @@ app.delete('/cars/:id', async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+
+//http://localhost:3001/cars/new
