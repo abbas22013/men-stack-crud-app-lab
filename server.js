be61a04eab6e7b8bae83da69a34a008adcb460d9
abbas <abbas22013@gmail.com>
2024-12-29 22:51:09 +0300

    Finish

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');  
 
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
+mongoose.connect(process.env.MONGODB_URI).then(() => {
   console.log("Connected to MongoDB");
 }).catch(err => {
   console.error("MongoDB connection error:", err);
